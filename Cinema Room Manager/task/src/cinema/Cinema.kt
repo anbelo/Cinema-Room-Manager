a4032b3bf742bcbbc32938f11fbc43b0f0a6485f
anbelo <andreij.belokurov@gmail.com>
2022-09-11 17:49:12 +0300

    Tickets (Stage 3/5)

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,5 +1,8 @@
 package cinema
 
+const val MIN_ROWS = 1
+const val MIN_SEATS = 1
+
 const val PRICE = 10
 const val DISCOUNT_PRICE = 8
 
@@ -7,14 +10,35 @@ fun main() {
     println("Enter the number of rows:")
     val rows = readln().toInt()
     println("Enter the number of seats in each row:")
-    val cols = readln().toInt()
-    val totalIncome = if (rows * cols <= 60) {
-        rows * cols * PRICE
+    val seats = readln().toInt()
+    printCinema(rows, seats)
+
+    println("Enter a row number:")
+    val row = readln().toInt()
+    println("Enter a seat number in that row:")
+    val seat = readln().toInt()
+
+    val ticketPrice = if (rows * seats <= 60) {
+        PRICE
     } else {
-        (rows / 2) * cols * PRICE + (rows - (rows / 2)) * cols * DISCOUNT_PRICE
+        if (row <= rows / 2) PRICE else DISCOUNT_PRICE
     }
-    println("""
-        Total income:
-        $$totalIncome
-    """.trimIndent())
+
+    println()
+    println("Ticket price: \$$ticketPrice")
+    printCinema(rows, seats, listOf(Pair(row, seat)))
+}
+
+fun printCinema(rows: Int, seats: Int, booked: List<Pair<Int, Int>> = emptyList()) {
+    println()
+    println("Cinema:")
+    println((MIN_SEATS..seats).joinToString(" ", "  "))
+    for (row in MIN_ROWS..rows) {
+        print("$row ")
+        for (seat in MIN_SEATS until seats) {
+            print(if (Pair(row, seat) in booked) "B " else "S ")
+        }
+        println("S")
+    }
+    println()
 }
